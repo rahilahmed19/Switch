787b66e9d1a846a62f82773a848e8f580d194f9e
rahilahmed19 <rahilahmed2002@gmail.com>
2023-03-16 13:18:44 -0400

    I forgot how to sort in descending order oop

--- help.cpp
+++ help.cpp
@@ -1,178 +1,38 @@
 #include <iostream>
 #include <time.h>
+#include <vector>
 using namespace std;
 
+class Player {
+	public:
+		int EC; string name;
+};
+
 int main() {
-	string name; bool knownToP1, knownToP2, knownToP3, knownToP4, knownToYou = false;
-
-void switchRankStart(Card& Temp) {
-	int power = Temp.power; string point;
-	switch (power) {
-		case 10:
-			point = "10";
-			cout << "|" << point << "     |  ";
-			break;
-		
-		case 11:
-			point = "J";
-			cout << "|" << point << "      |  ";
-			break;
-		
-		case 12:
-			point = "Q";
-			cout << "|" << point << "      |  ";
-			break;
-		
-		case 13:
-			point = "K";
-			cout << "|" << point << "      |  ";
-			break;
-		
-		case 1:
-			point = "A";
-			cout << "|" << point << "      |  ";
-			break;
-		
-		default:
-			cout << "|" << power << "      |  ";
-			break;
-	}
-}
-
-void switchRankEnd(Card& Temp) {
-	int power = Temp.power; string point;
-	switch (power) {
-		case 10:
-			point = "10";
-			cout << "|     " << point << "|  ";
-			break;
-		
-		case 11:
-			point = "J";
-			cout << "|      " << point << "|  ";
-			break;
-		
-		case 12:
-			point = "Q";
-			cout << "|      " << point << "|  ";
-			break;
-		
-		case 13:
-			point = "K";
-			cout << "|      " << point << "|  ";
-			break;
-		
-		case 1:
-			point = "A";
-			cout << "|      " << point << "|  ";
-			break;
-		
-		default:
-			cout << "|      " << power << "|  ";
-			break;
-	}
-}
-
-void printCard(Card& Temp) {
-	int rank = Temp.power; string suit = Temp.suit;
-	cout << " ———————   " << endl;
-	if (Temp.power >= 10 || Temp.power == 1) {
-		switchRankStart(Temp); cout << endl;
-	}
-	else {
-		cout << "|" << rank << "      |  " << endl;
-	}
-	cout << "|       |" << endl;
-	cout << "|   " << suit << "   |  " << endl;
-	cout << "|       |" << endl;
-	if (Temp.power >= 10 || Temp.power == 1) {
-		switchRankEnd(Temp); cout << endl;
-	}
-	else {
-		cout << "|      " << rank << "|  " << endl;
-	}
-	cout << " ———————   " << endl;
-}
-
-void printStack(vector<Card>& Stack) {
-	if (Stack.size() <= 1) { printCard(Stack.at(0)); }
-	else {
-		int i = Stack.size(); i--;
-		Card Temp = Stack.at(i - 1); int rank = Temp.power;
-		Card Temp2 = Stack.at(i); int rank2 = Temp.power; string suit2 = Temp.suit;
-		cout << " ——————— " << endl;
-		switchRankStart(Temp); cout << endl;
-		cout << "|  ———————   " << endl;
-		cout << "| "; switchRankStart(Temp2); cout << endl;
-		cout << "| |       |  " << endl;
-		cout << "| |   " << suit2 << "   |" << endl;
-		cout << " —|       |  " << endl;
-		cout << "  "; switchRankEnd(Temp2); cout << endl;
-		cout << "   ———————   " << endl;
-	}
-}
-
-void printHand() {
-	if (Hand.size() <= 1) { printCard(Hand.at(0)); }
+	Player P1; Player P2; Player P3; Player P4; Player lowest; 
 	
-	else {
-		Card Temp; int rank = Temp.power; string suit = Temp.suit;
-		for (int i = 0; i < Hand.size(); i++) { // Line 1
-			cout << " ———————   ";
-			if (i == Hand.size() - 1) { cout << endl; }
+	P1.name = "Player 1"; P2.name = "Player 2"; P3.name = "Player 3"; P4.name = "Player 4";
+	P1.EC = 4; P2.EC = 9; P3.EC = 6; P4.EC = 1;
+	
+	vector<Player> Players{P1, P2, P3, P4};
+	
+	srand(time(NULL));
+
+	for (int i = 1; i < Players.size(); i++) {
+		lowest = Players.at(i); int j = i - 1;
+		while (j >= 0 && Players.at(j).EC < lowest.EC) {
+			Players.at(j + 1) = Players.at(j); j--;
 		}
-		
-		for (int i = 0; i < Hand.size(); i++) { // Line 2
-			Temp = Hand.at(i); rank = Temp.power;
-			if (Temp.knownToYou) {
-				switchRankStart(Temp);
-			}
-			else {
-				cout << "|?      |  ";
-			}
-			if (i == Hand.size() - 1) { cout << endl; }
-		}
-		
-		for (int i = 0; i < Hand.size(); i++) { // Line 3
-			cout << "|       |  ";
-			if (i == Hand.size() - 1) { cout << endl; }
-		}
-		
-		for (int i = 0; i < Hand.size(); i++) { // Line 4
-			Temp = Hand.at(i); suit = Temp.suit;
-			if (Temp.knownToYou) {
-				cout << "|   " << suit << "   |  ";
-			}
-			else {
-				cout << "|   ?   |  ";
-			}
-			if (i == Hand.size() - 1) { cout << endl; }
-		}
-		
-		for (int i = 0; i < Hand.size(); i++) { // Line 5
-			cout << "|       |  ";
-			if (i == Hand.size() - 1) { cout << endl; }
-		}
-		
-		for (int i = 0; i < Hand.size(); i++) { // Line 6
-			Temp = Hand.at(i); rank = Temp.power;
-			if (Temp.knownToYou) {
-					switchRankEnd(Temp);
-			}
-			else {
-				cout << "|      ?|  ";
-			}
-			if (i == Hand.size() - 1) { cout << endl; }
-		}
-		
-		for (int i = 0; i < Hand.size(); i++) { // Line 7
-			if (i == Hand.size() - 1) { 
-				cout << " ——————— " << endl; 
-			}
-			else { cout << " ——————— , "; }
-		}
+		Players.at(j + 1) = lowest;
 	}
-}
+
+	cout << "\nIn first place, we have " << Players.at(0).name << " with " << Players.at(0).EC << " eliminations." << endl;
+	
+	cout << "\nIn second place, we have " << Players.at(1).name << " with " << Players.at(1).EC << " eliminations." << endl;
+	
+	cout << "\nIn third place, we have " << Players.at(2).name << " with " << Players.at(2).EC << " eliminations." << endl;
+	
+	cout << "\nAnd in fourth place, we have " << Players.at(3).name << " with " << Players.at(3).EC << " eliminations.\n" << endl;
 
   	return 0;
 }
